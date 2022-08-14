b036d82d82f76f20e2b84c763498f3bbe3953821
mmateusf <106560647+mmateusf@users.noreply.github.com>
2022-08-13 19:04:01 -0500

    first commit

---
+++ 5/src/main/java/com/mycompany/reto5java/controlador/ControladorMain.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.reto5java.controlador;
+
+import com.mycompany.reto5java.vista.frmPrimerInforme;
+import com.mycompany.reto5java.vista.frmPrincipal;
+
+
+public class ControladorMain {
+    public static void main(String[] args){
+        new frmPrincipal().setVisible(true);
+        
+        
+}
+}
